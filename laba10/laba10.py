b0ec36453a668f28ad9d2726430469d0d9bc97e5
KovalEgor <127386678+KovalEgor@users.noreply.github.com>
2025-03-17 20:37:11 +0900

    Create laba10.py
    1

---
+++ laba10/laba10.py
@@ -0,0 +1,90 @@
+import numpy as np
+
+class AntColony:
+    def __init__(self, distances, n_ants, n_best, n_iterations, decay, alpha=1, beta=1):
+        """
+        distances: матрица расстояний между городами
+        n_ants: количество муравьев
+        n_best: количество лучших муравьев, которые оставляют феромон
+        n_iterations: количество итераций
+        decay: скорость испарения феромона
+        alpha: влияние феромона
+        beta: влияние расстояния
+        """
+        self.distances = distances
+        self.pheromone = np.ones(self.distances.shape) / len(distances)
+        self.all_inds = range(len(distances))
+        self.n_ants = n_ants
+        self.n_best = n_best
+        self.n_iterations = n_iterations
+        self.decay = decay
+        self.alpha = alpha
+        self.beta = beta
+
+    def run(self):
+        shortest_path = None
+        all_time_shortest_path = (None, np.inf)
+        for i in range(self.n_iterations):
+            all_paths = self.gen_all_paths()
+            self.spread_pheromone(all_paths, self.n_best, shortest_path=shortest_path)
+            shortest_path = min(all_paths, key=lambda x: x[1])
+            if shortest_path[1] < all_time_shortest_path[1]:
+                all_time_shortest_path = shortest_path            
+            self.pheromone *= self.decay
+        return all_time_shortest_path
+
+    def spread_pheromone(self, all_paths, n_best, shortest_path):
+        sorted_paths = sorted(all_paths, key=lambda x: x[1])
+        for path, dist in sorted_paths[:n_best]:
+            for move in path:
+                self.pheromone[move] += 1.0 / self.distances[move]
+
+    def gen_path_dist(self, path):
+        total_dist = 0
+        for ele in path:
+            total_dist += self.distances[ele]
+        return total_dist
+
+    def gen_all_paths(self):
+        all_paths = []
+        for i in range(self.n_ants):
+            path = self.gen_path(0)
+            all_paths.append((path, self.gen_path_dist(path)))
+        return all_paths
+
+    def gen_path(self, start):
+        path = []
+        visited = set()
+        visited.add(start)
+        prev = start
+        for i in range(len(self.distances) - 1):
+            move = self.pick_move(self.pheromone[prev], self.distances[prev], visited)
+            path.append((prev, move))
+            prev = move
+            visited.add(move)
+        path.append((prev, start))
+        return path
+
+    def pick_move(self, pheromone, dist, visited):
+        pheromone = np.copy(pheromone)
+        pheromone[list(visited)] = 0
+
+        row = pheromone ** self.alpha * (( 1.0 / dist) ** self.beta)
+
+        norm_row = row / row.sum()
+        move = np.random.choice(self.all_inds, 1, p=norm_row)[0]
+        return move
+
+if __name__ == '__main__':
+    distances = np.array([
+        [np.inf, 2, 2, 5, 7],
+        [2, np.inf, 4, 8, 2],
+        [2, 4, np.inf, 1, 3],
+        [5, 8, 1, np.inf, 2],
+        [7, 2, 3, 2, np.inf]
+    ])
+
+    ant_colony = AntColony(distances, n_ants=10, n_best=3, n_iterations=100, decay=0.95, alpha=1, beta=2)
+    shortest_path, length = ant_colony.run()
+    print("Кратчайший путь: ", shortest_path)
+    print("Длина пути: ", length)
